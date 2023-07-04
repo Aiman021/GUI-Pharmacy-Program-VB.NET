e85ca081916f89df81c7805fc1aa210568717d57
Aiman Haroon <113941202+Aiman021@users.noreply.github.com>
2023-07-04 01:26:22 -0500

    Add files via upload

---
+++ Drugs.vb
@@ -0,0 +1,25 @@
+﻿'********************************************************************************************
+'* Name:       Aiman Haroon
+'* Class:      CIS-1510
+'* Assignment: Project 02 Fall 2020
+'* File:       frmMainDrugs.vb
+'* Purpose:    Pharmacy Drug Inventory Software for Create,Read, and edit items
+'********************************************************************************************
+Public Class Drugs
+
+    Public NDC As String
+    Public drugName As String
+    Public dosage As String
+    Public desc As String
+    Public unitPrice As Decimal
+    Public packagePrice As Decimal
+    Public count As Integer
+    Public dateReceived As String
+    Public supplier As String
+    Public category As String
+    Public drugType As String
+    Public inStock As Integer
+    Public unitQuantity As String
+
+
+End Class
---
+++ about.txt
@@ -0,0 +1,19 @@
+﻿'********************************************************************************************
+'* Name:       Aiman Haroon
+'* Class:      CIS-1510
+'* Assignment: Project 02 Fall 2020
+'* File:       frmMainDrugs.vb
+'* Purpose:    Pharmacy Drug Inventory Software for Create,Read,and edit items
+'********************************************************************************************
+
+Option Strict On
+Option Explicit On
+Option Infer Off
+Option Compare Binary
+
+Public Class frmAbout
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        Me.Close()
+    End Sub
+
+End Class
